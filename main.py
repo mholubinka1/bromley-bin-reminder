d76eb9f2b2564d7fea4d7ed1f05d5ba1eddedd69
Matt Holubinka <meholubinka@gmail.com>
2024-10-23 20:27:08 +0100

    config

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import logging.config
+import os
+import pathlib
+import sys
 from datetime import datetime
 from logging import Logger, getLogger
 
 from logging_config import APP_LOGGER_NAME, config
+from settings import get_settings
 
 logging.config.dictConfig(config)
 logger: Logger = getLogger(APP_LOGGER_NAME)
 
 logger.info("Starting bromley-bin-reminder.")
 
+CONFIG_PATH = os.path.join(pathlib.Path(__file__).parent.resolve(), "config.yml")
+
+try:
+    settings = get_settings(config_file_path=CONFIG_PATH)
+    print(settings.wasteworks_url)
+    print(settings.remind.target_emails)
+except Exception as e:
+    logger.critical(f"Error loading startup configurations: {e}.")
+    sys.exit(1)
+
+
 today = datetime.today().date()
-print(today)
